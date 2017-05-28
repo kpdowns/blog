27f21f34b84943c41f665753baa01902b60ea29a
Kevin Downs <kevindwns@gmail.com>
2017-05-28 17:55:13 +0200

    Update deployment script

---
+++ deploy.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+ECHO Rebuilding site
+rmdir /s /q _site
+SET "JEKYLL_ENV=production"
+call jekyll build
+
+ECHO
+ECHO Uploading to FTP
+winscp.com /command "open ftp://%BlogUploadUsername%:%BlogUploadPassword%@ftp.kevindowns.co.za/" "synchronize remote _site public_html -delete -transfer=automatic" "exit"
